950e3feb54e98c63565c1d8648b905bed2edbd38
PedroMakengo <pedromakengomiguel@gmail.com>
2023-07-19 11:22:45 +0100

    Adicionando as rotas

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-page class="flex flex-center">
+    <div>
+      <h5>Acesse o chat</h5>
+      {{ email }}
+      <q-input
+        rounded
+        outlined
+        v-model="email"
+        label="Informe o seu E-mail"
+        bg-color="white"
+        class="q-mb-md"
+      ></q-input>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+
+  data() {
+    return {
+      email: "",
+    };
+  },
+});
+</script>
